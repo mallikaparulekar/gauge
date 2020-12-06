03e8dd83a247ac0f023375ea804740797d7104b8
Mallika Parulekar <43099223+mkp2020@users.noreply.github.com>
2020-12-06 12:41:11 -0800

    Add files via upload

---
+++ venv/utils/datasets/fer.py
@@ -0,0 +1,24 @@
+import os
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+
+os.chdir("/Users/sharad/PycharmProjects/HackDukeML/venv")
+#path = os.getcwd()
+#print(os.listdir(path))
+
+def plot_example_images(plt):
+    img_size = 48
+    plt.figure(0, figsize=(12,20))
+    ctr = 0
+
+    for expression in os.listdir("train/"):
+        #print("expression", expression)
+        if expression != ".DS_Store":
+            for i in range(1,6):
+                ctr += 1
+                plt.subplot(7,5,ctr)
+                img = load_img("train/" + expression + "/" +os.listdir("train/" + expression)[i], target_size=(img_size, img_size))
+                plt.imshow(img, cmap="gray")
+
+    plt.tight_layout()
+    return plt
+
